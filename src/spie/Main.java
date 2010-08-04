e6e0149167cee831f9aae835968a9e00f11fc2f3
Vincent Xue <chrnovx@gmail.com>
2010-08-03 22:49:15 -0400

    "Fixed packaging problem with fatjar"

--- src/spie/Main.java
+++ src/spie/Main.java
@@ -1,12 +1,5 @@
 package spie;
 
-import java.awt.Image;
-import java.awt.Toolkit;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-
-import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 
 
@@ -18,13 +11,6 @@ public class Main {
 		//Create a JFrame
 		
 		JFrame frame= new JFrame("SPlicing Interactive Environment");
-		BufferedImage img = null;
-		try {
-			img = ImageIO.read(frame.getClass().getResource("/spie.gif"));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		frame.setIconImage(img);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		JSpliceViewGUI panel = new JSpliceViewGUI();
 		frame.add(panel);
